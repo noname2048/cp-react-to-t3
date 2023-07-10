5dfe53695a720187425020e81d0914a83b95625c
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-10 16:35:48 +0900

    feat: add test page for chartjs

---
+++ nextjs-page/src/pages/chartjs.tsx
@@ -0,0 +1,87 @@
+import { useApi } from "@/utils/use-api";
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  animations: {
+    duration: 0,
+  },
+  interaction: {
+    mode: "x" as const,
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Home Temperature and Humidity",
+    },
+  },
+  pointStyle: false,
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+    },
+    y1: {
+      type: "linear",
+      display: true,
+      position: "right",
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+
+export default function App() {
+  const { data, isLoading, isError } = useApi();
+  if (isLoading || isError || !data?.data) return <div>Loading or Error</div>;
+  const array = data?.data;
+
+  const labels = array.map((d) => d.ca);
+
+  const _data = {
+    labels,
+    datasets: [
+      {
+        label: "Temperature",
+        data: array.map((d) => d.temperature),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        yAxisID: "y",
+      },
+      {
+        label: "Humidity",
+        data: array.map((d) => d.humidity),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        yAxisID: "y1",
+      },
+    ],
+  };
+
+  return <Line options={options} data={_data} />;
+}
